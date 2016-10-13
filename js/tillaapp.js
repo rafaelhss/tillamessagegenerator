ff4c90ffc90152c484ba11437104d48349823e0e
rafael <rafael.soares@bcb.gov.br>
2016-10-13 20:06:30 -0300

    + calculo sedex

--- js/tillaapp.js
+++ js/tillaapp.js
@@ -1,6 +1,36 @@
 
 var app = angular.module("tillaApp", []);
 
+
+var atualizaTexto = function(postdata, valortag, prazotag, $scope, $http){
+    $scope.carregando = true;
+    $http.post("https://nocors.herokuapp.com/http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo", postdata, {
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+        }
+    ).success(function (data, status) {
+            $scope.carregando = false;
+            var x2js = new X2JS();
+            var xmlText = data;
+            var jsonObj = x2js.xml_str2json(xmlText);
+
+            console.log(jsonObj.cResultado.Servicos.cServico.Valor);
+
+            var prazo = 0 + jsonObj.cResultado.Servicos.cServico.PrazoEntrega;
+            var valor =  parseFloat("1.00") + parseFloat(jsonObj.cResultado.Servicos.cServico.Valor.replace(",",".")); //soma $1 pra caixa
+
+
+
+            $scope.texto = $scope.texto.replace(valortag, valor.toFixed(2)).replace(prazotag, prazo);
+
+
+        }).error(function (data, status) {
+            $scope.carregando = false;
+            $scope.prazo = -1;
+            $scope.valor = -1;
+
+        })
+}
+
 app.controller("tillaCtrl", function ($scope, $http) {
 
     $scope.texto = getTextoInicial();
@@ -17,10 +47,15 @@ app.controller("tillaCtrl", function ($scope, $http) {
 
 
 
+
     $scope.frete = function (cep) {
         console.log(cep);
 
-        if ($scope.texto.indexOf("[valor]") < 0 || $scope.texto.indexOf("[prazo]") < 0) {
+        if ($scope.texto.indexOf("[valor]") < 0 ||
+            $scope.texto.indexOf("[prazo]") < 0 ||
+            $scope.texto.indexOf("[valors]") < 0||
+            $scope.texto.indexOf("[prazos]") < 0) {
+
             $scope.errotag = true;
         }
         else {
@@ -28,35 +63,9 @@ app.controller("tillaCtrl", function ($scope, $http) {
 
             localStorage.setItem("texto", $scope.texto);
 
+            atualizaTexto(getDataPac(cep), '[valor]', '[prazo]', $scope, $http);
+            atualizaTexto(getDataSedex(cep), '[valors]', '[prazos]', $scope, $http)
 
-            $scope.carregando = true;
-            $http.post("https://nocors.herokuapp.com/http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrecoPrazo", getData(cep), {
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
-            }
-            ).success(function (data, status) {
-                $scope.carregando = false;
-                var x2js = new X2JS();
-                var xmlText = data;
-                var jsonObj = x2js.xml_str2json(xmlText);
-
-console.log(jsonObj.cResultado.Servicos.cServico.Valor);
-
-                var prazo = 0 + jsonObj.cResultado.Servicos.cServico.PrazoEntrega;
-                var valor =  parseFloat("1.00") + parseFloat(jsonObj.cResultado.Servicos.cServico.Valor.replace(",",".")); //soma $1 pra caixa
-
-
-
-                $scope.texto = $scope.texto.replace("[valor]", valor.toFixed(2)).replace("[prazo]", prazo);
-
-
-
-
-            }).error(function (data, status) {
-                $scope.carregando = false;
-                $scope.prazo = -1;
-                $scope.valor = -1;
-
-            })
         }
     }
 
@@ -79,13 +88,18 @@ app.directive('selectOnClick', ['$window', function ($window) {
     };
 }]);
 
-function getData(cep) {
-    return "nCdEmpresa=&sDsSenha=&nCdServico=41106&sCepOrigem=70873-060&sCepDestino=" + cep + "&nVlPeso=0.3&nCdFormato=1&nVlComprimento=16&nVlAltura=11&nVlLargura=16&nVlDiametro=0&sCdMaoPropria=&nVlValorDeclarado=0&sCdAvisoRecebimento=N";
+function getDataPac(cep) {
+    return "nCdEmpresa=&sDsSenha=&nCdServico=41106&sCepOrigem=70873-060&sCepDestino=" + cep + "&nVlPeso=0.3&nCdFormato=1&nVlComprimento=18&nVlAltura=9&nVlLargura=27&nVlDiametro=0&sCdMaoPropria=&nVlValorDeclarado=0&sCdAvisoRecebimento=N";
+}
+
+function getDataSedex(cep) {
+    return "nCdEmpresa=&sDsSenha=&nCdServico=40010 &sCepOrigem=70873-060&sCepDestino=" + cep + "&nVlPeso=0.3&nCdFormato=1&nVlComprimento=18&nVlAltura=9&nVlLargura=27&nVlDiametro=0&sCdMaoPropria=&nVlValorDeclarado=0&sCdAvisoRecebimento=N";
 }
 
 function getTextoInicial() {
     if(localStorage.getItem("texto") === null)
-        return "O frete para seu CEP fica $[valor]. O prazo de entrega é de até [prazo] dias úteis via PAC. Envio seguro com codigo de rastreio. Esse valor vale para até 20 caixas.";
+        return "O frete para seu CEP fica $[valor]. O prazo de entrega é de até [prazo] dias úteis via PAC. " +
+            "Envio seguro com codigo de rastreio. (Se preferir SEDEX, fica $[valors] e chega em [prazos] dias uteis.)";
     else
         return localStorage.getItem("texto");    
 }
